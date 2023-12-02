f2c7df0135dab334ea36a5685a8e1875fcb3d1ef
Success Enehikhuere <enehikhueresuccess@gmail.com>
2023-12-02 13:16:33 +0100

    feat; Add sass to project, and set up the sass 7-1 architecture

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+
+
+function App() {
+ 
+  return (
+    <>
+      <h2>hello tinyan baby love</h2>
+      <button>hello</button>
+      <button>hello</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/pxToRem.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const pxToRem = (px) => `${px/16}rem`
+
+export default pxToRem
